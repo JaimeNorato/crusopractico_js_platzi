9b7cf31fd61209684366470e99372966075feef0
Jaime Norato <jn901113jn@gmail.com>
2021-07-30 07:04:05 -0500

    ponderado

--- promedio.js
+++ promedio.js
@@ -41,3 +41,37 @@ function calcularModa(lista = []){
 
     return moda;
 }
+
+function ponderado(){
+    let sumOfCredits=0;
+    const notes = [
+        {
+            course: "Educación Física",
+            note: 10,
+            credit: 2,
+        },
+        {
+            course: "Programación",
+            note: 8,
+            credit: 5,
+        },
+        {
+            course: "Finanzas personales",
+            note: 7,
+            credit: 5,
+        },
+    ];
+
+    const notesWithCredit = notes.map(function (noteObject) {
+        sumOfCredits += noteObject.credit
+        return noteObject.note * noteObject.credit;
+    });
+
+    const sumOfNotesWithCredit = notesWithCredit.reduce(function (sum = 0, newVal) {
+            return sum + newVal;
+        }
+    );
+
+    return sumOfNotesWithCredit / sumOfCredits;
+
+}
